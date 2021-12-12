5e0fca09dcf70caf9fbe132e5e0e1c80b7c9626f
hisa <gwall59@gmail.com>
2021-12-12 10:00:28 +0900

    Kadai06_genreRankingを関数化

--- Kadai06_genreRanking.py
+++ Kadai06_genreRanking.py
@@ -6,30 +6,28 @@ import datetime
 import functions
 from config import *
 
+global keyword
+
 current_datetime = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
 current_date = datetime.datetime.now().strftime('%Y-%m-%d')
 
-functions.make_log('検索開始')
-keyword = input('検索したい商品名: ')
-
-GENRERANKING_FILE_PATH = f'./search/rakuten_genreranking_{keyword}_{current_datetime}.csv'
-os.makedirs(os.path.dirname(GENRERANKING_FILE_PATH), exist_ok=True)
+# os.makedirs(os.path.dirname(GENRERANKING_FILE_PATH), exist_ok=True)
 os.makedirs(os.path.dirname(LOG_FILE_PATH), exist_ok=True)
-
-REQUEST_ITEM_URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?'
 REQUEST_RANKING_URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?'
 
-params = {
+
+def check_counts_of_items_by_keyword(keyword, APP_ID):
+  REQUEST_ITEM_URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?'
+  params = {
     'keyword': keyword,
     'applicationId': APP_ID,
-}
-
-res = requests.get(REQUEST_ITEM_URL, params=params)
-result = res.json()
-total = result['count']
-if total == 0:
-  print('該当の商品は見つかりませんでした。\n検索を終了します。')
-else:
+  }
+  res = requests.get(REQUEST_ITEM_URL, params=params)
+  result = res.json()
+  total = result['count']
+  return total
+  
+def set_pages_by_total_items(total):
   # 検索された全件は何ページ分になるか
   pages = int(total / 30) + 1
   #取得可能な最大ページ数
@@ -37,7 +35,11 @@ else:
   if pages > MAX:
     pages = MAX
 
-  row = []
+  return pages
+
+def search_items_detail(keyword, pages):
+  REQUEST_RANKING_URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?'
+  rows = []
   try:
     functions.make_log('情報取得開始')
     for i in range(1, pages+1):
@@ -47,32 +49,35 @@ else:
           'page': i,
       }
 
-      res = requests.get(REQUEST_ITEM_URL, params=params)
+      res = requests.get(REQUEST_RANKING_URL, params=params)
       result = res.json()
       items_info = result['Items']
       for j, item_info in enumerate(items_info):
         item_name = item_info['Item']['itemName'][:30]
         item_genreId = item_info['Item']['genreId']
-        row.append([item_name,item_genreId])
+        rows.append([item_name,item_genreId])
       time.sleep(0.5)
   except:
     functions.has_error()
-    
+   
+  return rows 
+
+def find_genreId_mode(rows):
   col = ['Item','GenreId']
-  df = pandas.DataFrame(row, columns=col)
+  df = pandas.DataFrame(rows, columns=col)
+  id_mode = df['GenreId'].mode()[0]
+  print(f'検索した商品はジャンルID{id_mode}に属しています。\nジャンルID{id_mode}のランキングを取得します。')
   
-  genreId_mode = df['GenreId'].mode()[0]
-  print(f'検索した商品はジャンルID{genreId_mode}に属しています。\nジャンルID{genreId_mode}のランキングを取得します。')
-  
+  return id_mode
+
+def make_itemRanking_by_genreId_mode(id_mode):
   params = {
       'applicationId': APP_ID,
-      'genreId': genreId_mode,
+      'genreId': id_mode,
   }
-
   res = requests.get(REQUEST_RANKING_URL,params=params)
   result = res.json()
   items_info = result['Items']
-
   rows = []
   for item_info in items_info:
     item_rank = item_info['Item']['rank']
@@ -87,5 +92,42 @@ else:
   time.sleep(0.5)
 
   col = ['順位', '商品名','価格','商品URL','店名','店URL','ジャンルID']
-  df2 = pandas.DataFrame(rows, columns=col)
-  df2.to_csv(GENRERANKING_FILE_PATH, index=False, encoding='utf_8_sig')
+  df = pandas.DataFrame(rows, columns=col)
+  # GENRERANKING_FILE_PATH = f'./search/rakuten_genreranking_{keyword}_{current_datetime}.csv'
+  # df.to_csv(GENRERANKING_FILE_PATH, index=False, encoding='utf_8_sig')
+  return df
+
+def save_data_in_csv_file(data, path):
+  os.makedirs(os.path.dirname(path), exist_ok=True)
+  data.to_csv(path, index=False, encoding='utf_8_sig')
+
+def main():
+  functions.make_log('検索開始')
+  keyword = input('検索したい商品名: ')
+  total = check_counts_of_items_by_keyword(keyword, APP_ID)
+
+  if total == 0:
+    functions.make_log('該当の商品なし。検索終了。')
+    print('該当の商品は見つかりませんでした。\n検索を終了します。')
+  else:
+    pages = set_pages_by_total_items(total)
+    print(f'検索された商品:{total}件、約{pages}ページ')
+    functions.make_log(f'検索された商品:{total}件')
+    
+    try:
+      rows = []
+      functions.make_log('ランキング情報取得開始')
+      rows = search_items_detail(keyword, pages)
+      genreId_mode = find_genreId_mode(rows)
+      
+      genreranking_file_path = f'./search/rakuten_genreranking_{keyword}_{current_datetime}.csv'
+      data = make_itemRanking_by_genreId_mode(genreId_mode)
+      save_data_in_csv_file(data, genreranking_file_path)
+ 
+    except:
+      functions.has_error()
+    print('ランキングを作成しcsvファイルに保存しました。')
+    functions.make_log('ランキング作成完了')
+
+if __name__ == "__main__":
+    main()
